01e7515336303ee85f5e861b4c9949224237ea2f
Jason Wong <jasonkwh@gmail.com>
2016-02-17 22:26:45 +1100

    Dealt with memory warning
    - Remove all caches to dealt with the memory warnings

--- Browser/ViewController.swift
+++ Browser/ViewController.swift
@@ -669,6 +669,8 @@ class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate, UISc
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+        NSURLCache.sharedURLCache().removeAllCachedResponses()
     }
 }
 
